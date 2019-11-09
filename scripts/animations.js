4f948193b71a17087b80303167a4ba3394418c49
Kirk <mrkirkmorgan@gmail.com>
2019-11-09 13:08:05 -0500

    Added aspects of the about section
    The profile picture and the resume buttons have been added for the about-me section. The animations are causing the CSS to get a bit messy, so I might move it all over to JS. The buttons are not finished since the text that appears below them is not displaying properly. A "welcome" message was also added to the top of index.html for anyone who views the source code.

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,3 +1,4 @@
+// Ensures that the bobbing arrow disappears when the user isn't at the top of the page
 $(document).on("scroll", function() {
     var pageTop = $(document).scrollTop()
 
@@ -6,4 +7,12 @@ $(document).on("scroll", function() {
     } else {
         $(".arrow").fadeOut("slow");
     }
+})
+
+// Adds listeners to the outer-halo buttons so the active state can be switched on click
+$(document).ready(function() {
+    $(".outer-halo").click(function(e) {
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+    });
 })
